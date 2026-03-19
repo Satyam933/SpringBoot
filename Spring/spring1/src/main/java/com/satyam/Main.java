deb9d63762043e1d86f1b5a0651a6168ab8429d4
Satyam Kumar <satyamkumar811450@gmail.com>
2026-03-19 10:06:02 +0530

    update Main by closing context

--- Spring/spring1/src/main/java/com/satyam/Main.java
+++ Spring/spring1/src/main/java/com/satyam/Main.java
@@ -1,14 +1,14 @@
 package com.satyam;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         //Insane obj = new Insane(); we have to do it by spring.
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring1.xml");
-        Insane obj = (Insane) context.getBean("insane");
-        System.out.print("I'M Insane ");
-        obj.run();
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring1.xml")) {
+            Insane obj = (Insane) context.getBean("insane");
+            System.out.print("I'M Insane ");
+            obj.run();
+        }
     }
 }
