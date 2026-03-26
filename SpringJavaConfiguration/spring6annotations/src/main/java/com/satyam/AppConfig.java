b4faa4ec5088b83cebee9ac371f7c49e7d12a56d
Satyam Kumar <satyamkumar811450@gmail.com>
2026-03-26 16:11:41 +0530

    added How @Component & @ComponentScan annotations works

---
+++ SpringJavaConfiguration/spring6annotations/src/main/java/com/satyam/AppConfig.java
@@ -0,0 +1,9 @@
+package com.satyam;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+@ComponentScan("com.satyam")
+public class AppConfig {
+  
+}
---
+++ SpringJavaConfiguration/spring6annotations/src/main/java/com/satyam/Insane.java
@@ -0,0 +1,10 @@
+package com.satyam;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Insane {
+  public void runInsane() {
+    System.out.println("I'M Insane !");
+  }
+}
