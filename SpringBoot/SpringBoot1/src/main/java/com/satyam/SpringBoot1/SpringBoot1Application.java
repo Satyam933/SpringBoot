2af53e53da3666959e1982f5d5c1df705fcf9093
Satyam Kumar <satyamkumar811450@gmail.com>
2026-03-27 20:21:38 +0530

    added first spring project

---
+++ SpringBoot/SpringBoot1/src/main/java/com/satyam/SpringBoot1/SpringBoot1Application.java
@@ -0,0 +1,10 @@
+package com.satyam.SpringBoot1;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot1Application {
+
+	public static void main(String[] args) {
+		System.out.println("First Spring Boot Project !");
+	}
+}
