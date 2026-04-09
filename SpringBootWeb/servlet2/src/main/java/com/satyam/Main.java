47d980e2b141c4c699c9463323e5c80b035093ff
Satyam Kumar <satyamkumar811450@gmail.com>
2026-04-09 22:38:50 +0530

    updated Main with comments

--- SpringBootWeb/servlet2/src/main/java/com/satyam/Main.java
+++ SpringBootWeb/servlet2/src/main/java/com/satyam/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws LifecycleException{
         System.out.println("Hello world!");
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
+        tomcat.setPort(8080);// for setting port number if 8080 is in use, we can use 8081
         tomcat.getConnector();
         Context context = tomcat.addContext("", new java.io.File(".").getAbsolutePath());
         Tomcat.addServlet(context, "Servlet2", new Servlet2());
